16fb8ff4744ff9fb5a99ee968c13d29101949ed6
Manqasana-Lepota <59992928+Manqasana-Lepota@users.noreply.github.com>
2026-04-27 17:20:55 +0200

    git commit -m "Add Flask project structure with routes, templates and static files"

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+from flask import Config
+
+mysql = MySQL()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load Confog from config.py
+    app.config.from_object(Config)
+
+    # MySQL Initialization
+    mysql.init_app(app)
+
+    # Register Blueprints
+    from app.routes.main_routes import main_bp
+    app.register_blueprint(main_bp)
+
+    #from app.routes.auth_routes import auth_bp
+    #app.register_blueprint(auth_bp)
+
+    #from app.routes.admin_routes import admin_bp
+    #app.register_blueprint(admin_bp)
+
+    #from app.routes.doctor_routes import doctor_bp
+    #app.register_blueprint(doctor_bp)
+
+    #from app.routes.patient_routes import patient_bp
+    #app.register_blueprint(patient_bp)
+
+    return app
---
+++ config.py
@@ -0,0 +1,11 @@
+class Config:
+
+    #  MySQL Settings
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'password'
+    MYSQL_DB = 'clinic-booking-system'
+    MYSQL_CURSORCLASS = 'DictCursor'
+
+    # Flask Settings
+    SECRET_KEY = 'clinic_booking_secret_12345'
